0d38400f21191ac0bfc8ce7a7ecf76ec3f50ed71
Matt Triff <matt.triff@gmail.com>
2014-06-02 22:38:43 -0400

    Small fixes, commenting

--- server.js
+++ server.js
@@ -7,21 +7,42 @@ var database = mysql.createConnection({
 });
 database.connect();
 
-//CORS middleware
+//CORS Middleware, causes Express to allow Cross-Origin Requests
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
 
-    next();
+  next();
 }
 
 var app = express();
 app.use(express.bodyParser());
 app.use(allowCrossDomain);
 
+// Get all records associated with the given user
+app.post('/getRecords', function(request, response) {
+  console.log('Sending records');
+  if (!request.body.email || !request.body.password) {
+    return response.send(400, 'Missing log in information.');
+  }
+  getRecords(request, function(err, result) {
+    return response.send(200, result);
+  });
+});
+
+// Helper function to get all records for a given user
+function getRecords(request, callback) {
+  console.log('Retrieving records.')
+  var query = database.query('SELECT records.date, records.tsh, records.tg, records.synthroidDose '
+    + ' FROM records, user ' + 'WHERE records.user=user.email' + ' AND user.email=? AND user.password=PASSWORD(?)' + ' ORDER BY records.date DESC', [request.body.email,
+    request.body.password
+  ], callback);
+}
+
+// Updates the records in the database with the provided records
 app.post('/syncRecords', function(request, response) {
-  console.log('Save Record Request Received!');
+  console.log('Save Records Request Received.');
 
   if (!request.body.email || !request.body.password) {
     return response.send(400, 'Missing log in information.');
@@ -36,52 +57,39 @@ app.post('/syncRecords', function(request, response) {
     if (!result) {
       return response.send(400, 'Invalid user credentials.');
     }
-    var recordsQuery = database.query('SELECT records.date, records.tsh, records.tg, records.synthroidDose '
-      + ' FROM records, user '
-      + 'WHERE records.user=user.email AND user.email=?'
-      + ' ORDER BY records.date DESC', [request.body.email], function(err, records) {
-        var newRecords = request.body.newRecords;
-        var recordsToSave = [];
-        for (var i = 0; i < newRecords.length; i++) {
-          var newRecord = [request.body.email, newRecords[i].date, newRecords[i].tsh, newRecords[i].tg, newRecords[i].synthroidDose];
-          recordsToSave.push(newRecord);
-        }
-        var deleteQuery = database.query('DELETE FROM records WHERE user=?', [request.body.email], function(err, result) {
-          if(err) {
-            return response.send(400, 'Error occurred syncing records');
-          }
-          var insertQuery = database.query('INSERT INTO records (user, date, tsh, tg, synthroidDose) VALUES ?', [recordsToSave], function(err, result) {
-            console.log(insertQuery.sql);
-            if(err) {
-              return response.send(400, 'Error occurred syncing records');
-            }
-            return response.send(200, 'Records synced.');
-          });
-        })
+    getRecords(request, function() {
+      var newRecords = request.body.newRecords;
+      var recordsToSave = [];
+      for (var i = 0; i < newRecords.length; i++) {
+        var newRecord = [request.body.email, newRecords[i].date, newRecords[i].tsh, newRecords[i].tg, newRecords[i].synthroidDose];
+        recordsToSave.push(newRecord);
+      }
+      syncRecords(request, response, recordsToSave);
     });
   });
 });
 
-app.post('/getRecords', function(request, response) {
-  console.log('Sending records');
-
-  if (!request.body.email || !request.body.password) {
-    return response.send(400, 'Missing log in information.');
-  }
-
-  var query = database.query('SELECT records.date, records.tsh, records.tg, records.synthroidDose ' + ' FROM records, user ' + 'WHERE records.user=user.email' + ' AND user.email=? AND user.password=PASSWORD(?)' + ' ORDER BY records.date DESC', [request.body.email,
-    request.body.password
-  ], function(err, result) {
-    console.log(result);
-    console.log(query.sql);
-    return response.send(200, result);
+// Helper function to remove all old records and insert all new records
+//  We do this instead of a combination of UPDATE and
+//  INSERT statements to simplify this process
+function syncRecords(request, response, recordsToSave) {
+  var deleteQuery = database.query('DELETE FROM records WHERE user=?', [request.body.email], function(err, result) {
+    if(err) {
+      return response.send(400, 'Error occurred syncing records');
+    }
+    var insertQuery = database.query('INSERT INTO records (user, date, tsh, tg, synthroidDose) VALUES ?', [recordsToSave], function(err, result) {
+      if(err) {
+        return response.send(400, 'Error occurred syncing records');
+      }
+      return response.send(200, 'Records synced.');
+    });
   });
+}
 
-});
-
+// Uses a email address and password to retrieve a user from the database
 app.post('/login', function(request, response) {
   console.log('Logging user in');
-  console.log(request.body);
+
   if (!request.body.email || !request.body.password) {
     return response.send(400, 'Missing log in information.');
   }
@@ -91,40 +99,37 @@ app.post('/login', function(request, response) {
     + 'WHERE user.email=? AND user.password=PASSWORD(?)', [request.body.email,
     request.body.password
   ], function(err, result) {
-    console.log(result);
-    console.log(query.sql);
     if(!result.length) {
       return response.send(400, 'Invalid password and email provided.');
     }
     delete result[0].password;
     return response.send(200, result[0]);
   });
-
 });
 
+// Creates a new user in the database
 app.post('/saveNewUser', function(request, response) {
   console.log('New user being created.');
   var user = request.body.newUser;
-  var query = database.query('INSERT INTO user VALUES (?, PASSWORD(?), ?, ?, ?, ?, ?, ?, ?, ?)', [user.Email,
-    user.NewPassword,
-    user.FirstName,
-    user.LastName,
-    user.HealthCardNumber,
-    user.DOB,
-    user.CancerType,
-    user.CancerStage,
-    user.TshRange,
+  var query = database.query('INSERT INTO user VALUES (?, PASSWORD(?), ?, ?, ?, ?, ?, ?, ?, ?)', [user.email,
+    user.newPassword,
+    user.firstName,
+    user.lastName,
+    user.healthCardNumber,
+    user.dateOfBirth,
+    user.cancerType,
+    user.cancerStage,
+    user.tshRange,
     false // agreedToLegal
   ], function(err, result) {
     if (err) {
-      console.log(err);
-      console.log(query.sql);
       return response.send(400, 'An error occurred creating this user.');
     }
     return response.json(200, 'User created successfully!');
   });
 });
 
+// Same as front end code, used to format a Date object appropriately
 function formatDateOfBirth(dateString) {
   var date = new Date(dateString);
   var month = date.getMonth()+1;
@@ -135,10 +140,10 @@ function formatDateOfBirth(dateString) {
     ((''+day).length<2 ? '0' : '') + day;
 }
 
+// Update user information
 app.post('/updateUser', function(request, response) {
   console.log('User being updated.');
   var user = request.body;
-  console.log(user);
   if(!user.newPassword) {
     user.newPassword = user.password;
   }
@@ -157,13 +162,13 @@ app.post('/updateUser', function(request, response) {
   ], function(err, result) {
     if (err) {
       console.log(err);
-      console.log(query.sql);
       return response.send(400, 'An error occurred creating this user.');
     }
     return response.json(200, 'User created successfully!');
   });
 });
 
+// Close the database connection and server when the application ends
 process.on('SIGTERM', function() {
   console.log("Shutting server down.");
   database.end();
